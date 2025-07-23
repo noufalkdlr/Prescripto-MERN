cb644d73bd13699f9415e737e0ac8c45b58deb4d
noufalkdlr <noufalkakdlr@gmail.com>
2025-07-23 20:33:38 +0530

    create nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
-const App = () =>{
-  return(
-    <div className="text-yellow-800">App</div>
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import Doctors from "./Pages/Doctors";
+import About from "./Pages/About";
+import Login from "./Pages/Login";
+import Contact from "./Pages/Contact";
+import MyProfile from "./Pages/MyProfile";
+import MyAppointments from "./Pages/MyAppointments";
+import Navbar from "./components/Navbar";
+
+const App = () => {
+  return (
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar/>
+      <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/doctors" element={<Doctors/>} />
+      <Route path="/doctors/:speciality" element={<Doctors/>} />
+      <Route path="/login" element={<Login/>} />
+      <Route path="/about" element={<About/>} />
+      <Route path="/contact" element={<Contact/>} />
+      <Route path="/my-profile" element={<MyProfile/>} />
+      <Route path="/my-appointments" element={<MyAppointments/>} />
+      <Route path="/appointments/:docId" element={<MyAppointments/>} />
+    </Routes>
+    </div>
+    
   )
-}
+};
 
 export default App;
